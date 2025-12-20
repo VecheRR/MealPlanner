58edfd89de19fe650a750be5a489868cbf16a36a
vecher <vecherkovsky2003@mail.ru>
2025-12-20 13:17:31 +0300

    Initial Commit

---
+++ MealPlanner/MealPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlannerApp.swift
+//  MealPlanner
+//
+//  Created by  Vladislav on 20.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MealPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
