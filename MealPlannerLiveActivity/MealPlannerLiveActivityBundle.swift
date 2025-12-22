1951a06b7c9f7b453111f6dbe7fd47e8b54f4f32
vecher <vecherkovsky2003@mail.ru>
2025-12-22 10:52:12 +0300

    Add gitignore

---
+++ MealPlannerLiveActivity/MealPlannerLiveActivityBundle.swift
@@ -0,0 +1,18 @@
+//
+//  MealPlannerLiveActivityBundle.swift
+//  MealPlannerLiveActivity
+//
+//  Created by  Vladislav on 20.12.2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MealPlannerLiveActivityBundle: WidgetBundle {
+    var body: some Widget {
+        MealPlannerLiveActivity()
+        MealPlannerLiveActivityControl()
+        MealPlannerLiveActivityLiveActivity()
+    }
+}
